f10408b152fd986e11e4c4154cbfdf445db004c6
Taha El khaoua <TahaElkhaoua.php@gmail.com>
2020-07-21 02:07:48 +0100

    minified and optimized files

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,39 @@
-(function(win, doc){
+!function(e,n){e.sendMessage=async function(){var e=n.querySelector("#sent"),a=n.querySelector(".contact__container"),t=document.querySelector("#name").value,o=document.querySelector("#email").value,c=document.querySelector("#message").value;a.classList.add("contact__container--folded"),e.value="sending...",e.disabled="true";var l=new URLSearchParams;l.append("name",t),l.append("email",o),l.append("message",c);var r=await(await fetch("/sendMail",{method:"POST",body:l})).text();e.value=r},e.scrollToElement=function(e){n.querySelector(e).scrollIntoView()},e.openUrl=function(e){window.open(e)}}(window,document);
+// (function(win, doc){
 
-    win.sendMessage = async function(){
-        //Style
-        var submitBtn = doc.querySelector('#sent');
-        var content = doc.querySelector('.contact__container');
+//     win.sendMessage = async function(){
+//         //Style
+//         var submitBtn = doc.querySelector('#sent');
+//         var content = doc.querySelector('.contact__container');
 
-        var name = document.querySelector('#name').value;
-        var email = document.querySelector('#email').value;
-        var message = document.querySelector('#message').value;
+//         var name = document.querySelector('#name').value;
+//         var email = document.querySelector('#email').value;
+//         var message = document.querySelector('#message').value;
 
-        content.classList.add("contact__container--folded");
+//         content.classList.add("contact__container--folded");
 
-        submitBtn.value = "sending...";
-        submitBtn.disabled = "true";
-        //Functionality
+//         submitBtn.value = "sending...";
+//         submitBtn.disabled = "true";
+//         //Functionality
 
-        var data = new URLSearchParams();
+//         var data = new URLSearchParams();
 
-        data.append('name', name);
-        data.append('email', email);
-        data.append('message', message);
+//         data.append('name', name);
+//         data.append('email', email);
+//         data.append('message', message);
 
-      var res = await (await fetch('/sendMail', {
-        method: 'POST',
-        body: data
-      })).text(); 
-        submitBtn.value = res;
-    }
-    win.scrollToElement = function(element){
-        var ele = doc.querySelector(element);
-        ele.scrollIntoView();
-    }
-    win.openUrl = function(customUrl){
-        window.open(customUrl);
-    }
-})(window, document);
+//       var res = await (await fetch('/sendMail', {
+//         method: 'POST',
+//         body: data
+//       })).text(); 
+//         submitBtn.value = res;
+//     }
+//     win.scrollToElement = function(element){
+//         var ele = doc.querySelector(element);
+//         ele.scrollIntoView();
+//     }
+//     win.openUrl = function(customUrl){
+//         window.open(customUrl);
+//     }
+// })(window, document);
 
